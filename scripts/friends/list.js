d1ab5bb18df5b1576bd6dc12e4b677a91d2ba12a
ZiAzusa <64982342+ZiAzusa@users.noreply.github.com>
2025-09-28 15:21:54 +0800

    Create list.js

---
+++ scripts/friends/list.js
@@ -0,0 +1,21 @@
+const linksData = [
+  {
+    name: "悠笙の开发日记",
+    url: "https://www.iyoroy.cn/",
+    avatar: "https://www.iyoroy.cn/usr/uploads/2025/07/1725584663.jpg",
+    slogan: "愿世间万物都能被温柔以待"
+  },
+  {
+    name: "筱莱Ceale的摇曳时间",
+    url: "https://ceale.moe/",
+    avatar: "https://avatars.githubusercontent.com/u/93030615?v=4",
+    slogan: "也许是只绵羊（？）"
+  }
+  // 在这里添加更多链接...
+  // {
+  //   name: "新网站",
+  //   url: "https://example.com",
+  //   avatar: "https://example.com/avatar.jpg",
+  //   slogan: "网站的一句描述"
+  // }
+];
